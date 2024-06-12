0cbf5ff23700f67e20b9b7210f5cb01f048793e9
Numan Ibn Mazid <numanibnmazid@gmail.com>
2024-06-12 15:48:43 +0600

    CUDA Installation added

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,47 @@
-FROM python:3.11-slim
+FROM flink:1.17.1
+
+# Set environment variables
+ENV CONDA_INSTALLER_URL=https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh \
+    MINICONDA_DIR=/opt/miniconda \
+    CONDA=/opt/miniconda/bin/conda \
+    PYTHON=/opt/miniconda/envs/env/bin/python
+
+# environment variables
+ENV NVIDIA_VISIBLE_DEVICES=all
+ENV NVIDIA_DRIVER_CAPABILITIES=all
+ENV NVIDIA_REQUIRE_CUDA "cuda>=12.2"
+
+# Update and install dependencies
+RUN apt-get update -y && \
+    apt-get install -y build-essential && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
 
 WORKDIR /app
+
+# Download and install Miniconda
+RUN wget $CONDA_INSTALLER_URL -O miniconda.sh && \
+    /bin/bash miniconda.sh -b -p $MINICONDA_DIR && \
+    rm miniconda.sh
+
+# Create a new environment named 'env'
+COPY environment.yml .
+RUN $CONDA env create -f environment.yml && \
+    $CONDA clean -afy
+
+# Ensure conda is on PATH
+ENV PATH="$MINICONDA_DIR/bin:$PATH"
+
+# Activate the 'env' environment
+SHELL ["conda", "run", "-n", "env", "/bin/bash", "-c"]
 
 COPY ./requirements.txt ./
 
 RUN pip install --no-cache-dir -r requirements.txt
 
-COPY entrypoint.sh /entrypoint.sh
+COPY entrypoint-ollama.sh /entrypoint-ollama.sh
+RUN chmod +x /entrypoint-ollama.sh
 
-RUN chmod +x /entrypoint.sh
+ENV LD_LIBRARY_PATH="$MINICONDA_DIR/envs/env/lib:${LD_LIBRARY_PATH}"
 
-CMD ["python", "main.py"]
+CMD ["/opt/miniconda/envs/env/bin/python", "main.py"]
--- main.py
+++ main.py
@@ -1,72 +1,81 @@
-import ollama
+from ollama import Client
+import time
 
-client = ollama.Client(host='ollama')
+client = Client(host='ollama')
 
 prompt = """
 Audio Transcription Input Data:
-
-{
+---
+{{
    "segments":[
-      {
-         "start":"2024-05-25 18:09:06.96",
-         "end":"2024-05-25 18:09:08.68",
-         "text":"Stability now is change."
-      },
-      {
-         "start":"2024-05-25 18:09:08.68",
-         "end":"2024-05-25 18:09:14.60",
-         "text":"It's strange to say that in 2024, but a stable economy has to be the first step of an incoming Labour government."
-      },
-      {
-         "start":"2024-05-25 18:09:15.32",
-         "end":"2024-05-25 18:09:20.84",
-         "text":" and that is what we'll make an absolute priority if we're privileged enough to come in to serve."
-      },
-      {
-         "start":"2024-05-25 18:09:22.96",
-         "end":"2024-05-25 18:09:31.88",
-         "text":" Visiting a supermarket in London, the woman who's pitching to be in charge of the public finances, Rachel Reeves, promised she would never play fast and loose with taxpayers' money."
-      },
-      {
-         "start":"2024-05-25 18:09:32.52",
-         "end":"2024-05-25 18:09:39.64",
-         "text":"All parties received a warning from the Independent Institute for Fiscal Studies today to be open with voters about the economic challenges ahead."
-      }
+  	{{
+     	"start": "2024-05-25 18:09:06.96",
+     	"end": "2024-05-25 18:09:08.68",
+     	"text": "Stability now is change."
+  	}},
+  	{{
+     	"start": "2024-05-25 18:09:08.68",
+     	"end": "2024-05-25 18:09:14.60",
+     	"text": "It's strange to say that in 2024, but a stable economy has to be the first step of an incoming Labour government."
+  	}},
+  	{{
+     	"start": "2024-05-25 18:09:15.32",
+     	"end": "2024-05-25 18:09:20.84",
+     	"text": " and that is what we'll make an absolute priority if we're privileged enough to come in to serve."
+  	}},
+  	{{
+     	"start": "2024-05-25 18:09:22.96",
+     	"end": "2024-05-25 18:09:31.88",
+     	"text": " Visiting a supermarket in London, the woman who's pitching to be in charge of the public finances, Rachel Reeves, promised she would never play fast and loose with taxpayers' money."
+  	}},
+  	{{
+     	"start": "2024-05-25 18:09:32.52",
+     	"end": "2024-05-25 18:09:39.64",
+     	"text": "All parties received a warning from the Independent Institute for Fiscal Studies today to be open with voters about the economic challenges ahead."
+  	}}
    ]
-}
-
-Here is the defination of audio transcription data:
+}}
+---
+Here is the definition of the data:
 
 {{
-	"segments": [
-		{{
-			"start": <Start of the segment>,
-			"end": <End of the segment>,
-			"text": <Actual text of that segment>
-		}}
-	]
+    "segments": [
+   	 {{
+   		 "start": <Start time of the segment>,
+   		 "end": <End time of the segment>,
+   		 "text": <Actual text of that segment>
+   	 }}
+    ]
 }}
 
-Can you please identify only the unique topics from the segments array of audio transcription data as below json format? Remember a topic might contain multiple items from segments array.
+Your task is to identify unique topics from the segments array and produce results in the given JSON format. Not necessarily each segment would be a unique topic, a topic might contain multiple items from the segments array.
+Do not generate any code or don't provide any concept of doing this.
 
+REQUIRED JSON FORMAT:
+---
 [
-	{{
-		"topic_id": <incremental id>,
-		"topic_name": <unique identifier that describes the topic>,
-		"concatenated_text": <concatenated text of topic segments>
-		"topic_summary": <short description of the topic>,
-		"position_in_segment": <position of this segment in segments array. Example: [<start_index>:<end_index>]>
-	}}
+    {{
+   	 "topic_id": <incremental id>,
+   	 "topic_name": <unique identifier that describes the topic>,
+   	 "concatenated_text": <concatenated text of each topic segment>
+   	 "topic_summary": <short description of the topic>,
+   	 "position_in_segments_array": <position of segments in segments array. Example: [0, 1, 2]>
+    }}
 ]
-Please answer in given json format.
+---
+
+REMEMBER ONLY ANSWER IN THE GIVEN JSON FORMAT. NOTHING MORE.
 """
 
 print("Generating output for the prompt: \n", prompt)
 
+start_time = time.perf_counter()
+
 response = client.generate(model='llama2', prompt=prompt)
 
+end_time = time.perf_counter()
+
 actual_response = response["response"]
-total_duration = response["total_duration"]
 
 print("\n Actual Response: \n", actual_response)
-print("\n Total Duration: \n", total_duration)
+print("\n Total Duration: \n", end_time - start_time, "seconds")
